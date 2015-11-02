85822e4a7ce055b7be3e24e0671e42becd5de773
JWillis45 <josephwillis@Josephs-MacBook-Pro.local>
2015-11-01 18:54:18 -0800

    added feature post to home page
    made the feature post the main image on the home page

--- index.php
+++ index.php
@@ -1,22 +1,76 @@
 <?php get_header(); ?>
 
 
-<?php query_posts('showposts=2'); ?>
 
-<?php if(have_posts()): while (have_posts()) : the_post(); ?>
+<?php 
+
+$args = array ('post_type' => 'features');
+
+$features = new WP_Query( $args );
+
+
+if ($features -> have_posts()) : while ($features -> have_posts()) : $features -> the_post(); 
+
+
+?>
+
 <div class="row">
 	<div class="col-xs-12">
       <div class="main-image">
       	<a href="<?php the_permalink() ?>">
-      		
-      		<?php the_content();
-?>
+     
+ 
+
+<div class="row">
+  <div class="col-xs-12 feature-image">
+    <?php the_content();?>
+  </div>
+</div>
+
+<div class="row">     
+    <div class="col-xs-12 category-box">
+      <?php the_title();?>
+    </div>
+</div>
+
+  </div>
       	</a>
+      </div>
+    </div>
+
+
+ 	
+
+
+  <?php endwhile; else: ?>
+    <p>Sorry, no pages matched your criteria.</p>
+<?php endif; ?>
+
+
+
+
+<?php if(have_posts()): while (have_posts()) : the_post(); ?>
+
+<div class="row">
+  <div class="col-xs-12">
+      <div class="main-image">
+        <a href="<?php the_permalink() ?>">
+
+          <?php the_title();?>
+          
+          <?php the_content();?>
+
+
+        </a>
       </div>
     </div>
 </div>
 
+  
 
-<?php endwhile; endif; ?>
+
+  <?php endwhile; else: ?>
+    <p>Sorry, no pages matched your criteria.</p>
+<?php endif; ?>
 
 <?php get_footer(); ?>
